8777d28a00a413be0abf5b1155aa3f7e67416cf2
Raz Friman <raz.friman@razfriman.com>
2018-10-31 10:33:10 +1100

    Remove quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { QuillModule } from 'ngx-quill';
 import { AppComponent } from './app.component';
 import { CanvaElementViewerComponent } from './canva-element-viewer/canva-element-viewer.component';
 import { CanvaPageViewerComponent } from './canva-page-viewer/canva-page-viewer.component';
@@ -20,7 +19,6 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     FormsModule,
-    QuillModule,
     ReactiveFormsModule,
     MonacoEditorModule.forRoot()
   ],
